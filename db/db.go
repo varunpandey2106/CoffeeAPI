04405dfff6682ee71fcfae6792e832e87c85fee6
varunpandey2106 <varun.pandey2106@gmail.com>
2024-01-22 22:07:19 +0530

    db connections

--- db/db.go
+++ db/db.go
@@ -2,8 +2,19 @@ package db
 
 import (
 	"database/sql"
+	"time"
 )
 
 type DB struct{
 	DB * sql.DB
+}
+
+var dbConn= &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn =5 
+const maxDbLifeime= 5* time.Minute
+
+func ConnectPostgres(dsn string) (*DB, error){
+	
 }
