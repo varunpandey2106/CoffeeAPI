3d12078ad8fa69f6036bc61d3649cfe729d04f37
varunpandey2106 <varun.pandey2106@gmail.com>
2024-01-23 19:33:15 +0530

    json response

--- services/main.go
+++ services/main.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"time"
+	"database/sql"
 )
 
 
---
+++ services/response.go
@@ -0,0 +1,8 @@
+package services
+
+type JsonResponse struct{
+	Error bool `json:"error"`
+	Message string `json:"message"`
+    Data interface{} `json:"data,omitresponse"`
+
+}
