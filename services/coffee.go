0de254e80f69992e6b8d2585ed9f1da7ef2008c3
varunpandey2106 <varun.pandey2106@gmail.com>
2024-01-23 19:18:31 +0530

    coffee.go init

--- services/coffee.go
+++ services/coffee.go
@@ -1,2 +1,18 @@
 package services
 
+import(
+	"time"
+)
+
+type Coffee struct {
+    ID string `json:"id"`
+    Name string `json:"name"`
+    Roast string `json:"roast"`
+    Image string `json:"image"`
+    Region string `json:"region"`
+    Price float32 `json:"price"`
+    GrindUnit int16 `json:"grind_unit"` 
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+}
+
