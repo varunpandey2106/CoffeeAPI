799f0fb1de512a1ebbe7829fd76244785e6a3379
varunpandey2106 <varun.pandey2106@gmail.com>
2024-01-24 20:24:47 +0530

    read json

--- helpers/helper.go
+++ helpers/helper.go
@@ -3,6 +3,8 @@ package helpers
 import (
 	"log"
 	"os"
+	"net/http"
+	"encoding/json"
 )
 
 type Envelope map[string] interface{}
@@ -15,4 +17,17 @@ type Message struct {
 
 var infoLog=log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 var errorLog=log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576
+	r.Body=http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+    err := dec.Decode(data)
+	if err != nil {
+        return err
+    }
+
+
+
+}
 
