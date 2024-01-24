e74016492f96fa51a7ecabdbca98b45692bf374f
varunpandey2106 <varun.pandey2106@gmail.com>
2024-01-24 20:34:50 +0530

    write json completed

--- helpers/helper.go
+++ helpers/helper.go
@@ -37,10 +37,24 @@ func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 
 }
 
-func WriteJSON(w http.ResponseWriter, status int, data interface{}, ...http.Header)error{
-	out, err := json.MarshalIndent(data, "", "\t")
-	if err != nil {
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+    out, err := json.MarshalIndent(data, "", "\t")
+    if err != nil {
         return err
     }
+    if len(headers) > 0 {
+        for key, value := range headers[0] {
+            w.Header() [key] = value
+        }
+    }
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+    _, err = w.Write(out)
+
+	if err!= nil{
+		return err
+	}
+
+	return nil
 }
 
